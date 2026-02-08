84ddae3070207b3b774a94382add49bf74779750
abcdefghij650-jpg <abcdefghij650@gmail.com>
2026-02-09 02:16:47 +0900

    Update watch.py

--- watch.py
+++ watch.py
@@ -1,11 +1,21 @@
 import requests
+import os
 
 URL = "https://nft.rakuten.co.jp/marketplace/?type=ticket&provider=nogizaka"
+BOT_TOKEN = os.environ["BOT_TOKEN"]
+CHAT_ID = os.environ["CHAT_ID"]
+
+def notify(msg):
+    requests.post(
+        f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
+        data={"chat_id": CHAT_ID, "text": msg}
+    )
 
 def main():
-    r = requests.get(URL)
+    r = requests.get(URL, timeout=10)
     if "該当するチケットは見つかりません" not in r.text:
-        raise Exception("TICKET FOUND")
+        notify("🚨 楽天NFTチケット出現！\n" + URL)
+        raise Exception("FOUND")
 
 if __name__ == "__main__":
     main()
